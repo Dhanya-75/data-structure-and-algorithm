dac0eccd2d93a18674262c27f8f2df12f24045b6
Dhanya M <dhanyamurugan57@gmail.com>
2025-12-30 04:14:18 +0530

    Add Two Sum solution in C

---
+++ leetcode/two sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int s;
+    scanf("%d",&s);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]+arr[j]==s)
+            {
+                printf("%d %d ",i,j);
+            }
+        }
+    }
+    return 0;
+}
